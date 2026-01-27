d5cfd2a7689d502f6476f73c6da0392a359fbe31
Istvan Vig <istvan.vig@parphis.hu>
2026-01-27 17:47:39 +0100

    Get lang code from session done

--- src/themes/omikk/app/shared/object-list/community-list-element/community-list-element.component.ts
+++ src/themes/omikk/app/shared/object-list/community-list-element/community-list-element.component.ts
@@ -1,5 +1,8 @@
 import { NgIf } from '@angular/common';
-import { Component } from '@angular/core';
+import { 
+  Component,
+  OnInit,
+} from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 import { Community } from '../../../../../../app/core/shared/community.model';
@@ -7,19 +10,40 @@ import { Context } from '../../../../../../app/core/shared/context.model';
 import { ViewMode } from '../../../../../../app/core/shared/view-mode.model';
 import { listableObjectComponent } from '../../../../../../app/shared/object-collection/shared/listable-object/listable-object.decorator';
 import { CommunityListElementComponent as BaseComponent } from '../../../../../../app/shared/object-list/community-list-element/community-list-element.component';
+import { DSONameService } from '../../../../../../app/core/breadcrumbs/dso-name.service';
+import { LocaleService } from '../../../../../../app/core/locale/locale.service';
 
-@listableObjectComponent(Community, ViewMode.ListElement, Context.Any, 'custom')
+@listableObjectComponent(Community, ViewMode.ListElement, Context.Any, 'omikk')
 
 @Component({
   selector: 'ds-community-list-element',
   // styleUrls: ['./community-list-element.component.scss'],
   styleUrls: ['../../../../../../app/shared/object-list/community-list-element/community-list-element.component.scss'],
-  // templateUrl: './community-list-element.component.html'
-  templateUrl: '../../../../../../app/shared/object-list/community-list-element/community-list-element.component.html',
+  templateUrl: './community-list-element.component.html',
+  //templateUrl: '../../../../../../app/shared/object-list/community-list-element/community-list-element.component.html',
   standalone: true,
   imports: [NgIf, RouterLink],
 })
 /**
  * Component representing a list element for a community
  */
-export class CommunityListElementComponent extends BaseComponent {}
+export class CommunityListElementComponent extends BaseComponent {
+  currentLang: string;
+
+  constructor(
+    public dsoNameService: DSONameService,
+    public localeService: LocaleService,) {
+    super(dsoNameService);
+  }
+
+  ngOnInit() {
+    // this.object.allMetadata("dc.title");
+    this.localeService.getCurrentLanguageCode().subscribe(code => {
+      this.currentLang = code;
+    });
+  }
+
+  getName() {
+    return this.dsoNameService.getName(this.object);
+  }
+}
